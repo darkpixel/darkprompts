459310ebeb0936eb13a03fdb5a796fde05e94110
Aaron C. de Bruyn <aaron@heyaaron.com>
2024-03-17 14:04:29 -0700

    Add DarkCheckpointRandomizer and DarkCheckpointSwitcher

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ from .gpl3 import DarkLoraTagLoader
 from .modules import faces
 from .modules import prompts
 from .modules import files
+from .modules import checkpoints
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +19,8 @@ NODE_CLASS_MAPPINGS = {
     "DarkLoRALoader": DarkLoraTagLoader,
     "DarkFaceIndexShuffle": faces.DarkFaceIndexShuffle,
     "DarkFaceIndexGenerator": faces.DarkFaceIndexGenerator,
+    "DarkCheckpointRandomizer": checkpoints.DarkCheckpointRandomizer,
+    "DarkCheckopintSwitcher": checkpoints.DarkCheckpointSwitcher,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -27,6 +30,8 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "DarkLoRALoader": "Dark LoRA Loader",
     "DarkFaceIndexShuffle": "Dark Face Index Shuffle",
     "DarkFaceIndexGenerator": "Dark Face Index Generator",
+    "DarkCheckpointRandomizer": "Dark Checkpoint Randomizer",
+    "DarkCheckpointSwitcher": "Dark Checkpoint Switcher",
 }
 
 __all__ = [NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS]
